59950b0ddc895c38e06a38b9b59308870dc02b80
wardseptember <wardseptember@gmail.com>
2018-09-20 07:30:30 +0800

    Site updated: 2018-09-20 07:30:30

--- photos/ins.js
+++ photos/ins.js
@@ -81,7 +81,8 @@
 
     /**
      * @name impush-client 
-     * @description 这个项目让我发家致富�?     * @date 2016-12-1
+     * @description 这个项目让我发家致富…
+     * @date 2016-12-1
      */
 
     var _collection = [];
@@ -128,7 +129,7 @@
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
             </figure>';
         }
-        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '�?em>' + data.month + '�?/em></h1>\
+        ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
         <ul class="img-box-ul">' + liTmpl + '</ul>\
         </section>';
       }
